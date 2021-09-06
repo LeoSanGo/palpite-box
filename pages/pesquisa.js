317b6b06192ac2de0b650dd0f1871c0a63d6ba35
LeoSanGo <souoleo@gmail.com>
2021-09-06 15:55:55 -0300

    modifiead file pesquisa.js

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -5,11 +5,11 @@ const Pesquisa = () => {
   return (
     <div className='pt-6'>
       <h1 className='text-center font-bold my-4 text-2xl'>Criticas e Sugestões</h1>
-      <p className='text-center'>O restaurante X sempre busca por atender melhor seus clientes.<br/>
+      <p className='text-center mb-6'>O restaurante X sempre busca por atender melhor seus clientes.<br/>
         Por isso estamos sempre abertos a ouvir a sua opinião</p>
-      <div className='w-48 mx-auto bg-red-100'>
-        <label>Seu nome:</label>
-        <input type='text' className='p-4 block shadow bg-blue-100 m-2'/>
+      <div className='w-1/5 mx-auto '>
+        <label className='font-bold'>Seu nome:</label>
+        <input type='text' className='p-4 block shadow bg-blue-100 m-2 rounded'/>
       </div>
     </div>
   );
